1e96f39f9f2ab19cbacbd8c943d4a89e5d1ea3ce
Michał Janiszewski <janisozaur@gmail.com>
2011-04-30 00:26:41 +0200

    add support for ifft, quite hackish and due to refactoring and fixes does not give expected results yet

--- transformwindow.cpp
+++ transformwindow.cpp
@@ -2,16 +2,20 @@
 #include "ui_transformwindow.h"
 #include "displaywindow.h"
 #include "filterinterface.h"
+#include "fft.h"
+#include "colorparser.h"
+#include "photowindow.h"
 
 #include <QDebug>
 
 Q_DECLARE_METATYPE(QUuid)
 
-TransformWindow::TransformWindow(QImage magnitude, QImage phase, QString title, QWidget *parent) :
+TransformWindow::TransformWindow(QImage magnitude, QImage phase, ComplexArray *ca, QString title, QWidget *parent) :
 	DisplayWindow(parent),
 	ui(new Ui::TransformWindow),
 	mMagnitudeImage(magnitude),
-	mPhaseImage(phase)
+	mPhaseImage(phase),
+	mCA(ca)
 {
 	constructorInternals(title);
 }
@@ -19,6 +23,7 @@ TransformWindow::TransformWindow(QImage magnitude, QImage phase, QString title, 
 TransformWindow::~TransformWindow()
 {
     delete ui;
+	delete mCA;
 }
 
 void TransformWindow::constructorInternals(QString title)
@@ -31,6 +36,10 @@ void TransformWindow::constructorInternals(QString title)
 	mFiltersMenu = menuBar()->addMenu("Filters");
 
 	connect(mFiltersMenu, SIGNAL(triggered(QAction*)), this, SLOT(applyFilter(QAction*)));
+
+	QAction *fftAction = new QAction("Invert", this);
+	connect(fftAction, SIGNAL(triggered()), this, SLOT(invert()));
+	menuBar()->addAction(fftAction);
 }
 
 void TransformWindow::appendFilter(FilterInterface *filter)
@@ -54,4 +63,66 @@ void TransformWindow::applyFilter(QAction *action)
 		DisplayWindow *dw = filter->apply(windowTitle());
 		q_check_ptr(dw)->show();
 	}
+}
+
+void TransformWindow::invert()
+{
+	FFT fft;
+	int layers = mCA->shape()[0];
+	int w = mCA->shape()[1];
+	int h = mCA->shape()[2];
+	ComplexArray *ca = new ComplexArray(boost::extents[layers][w][h]);
+	*ca = *mCA;
+	fft.perform(ca, true);
+	QImage::Format format = mPhaseImage.format();
+	ColorParser cp(format);
+	QImage result(w, h, format);
+	if (format == QImage::Format_Indexed8 || format == QImage::Format_Mono) {
+		QVector<QRgb> colors;
+		colors.reserve(256);
+		for (int i = 0; i < 256; i++) {
+			colors << qRgb(i, i, i);
+		}
+		result.setColorTable(colors);
+	}
+	for (unsigned int i = 0; i < ca->shape()[0]; i++) {
+		qreal min = 0;
+		qreal max = 0;
+		for (unsigned int j = 0; j < ca->shape()[1]; j++) {
+			for (unsigned int k = 0; k < ca->shape()[2]; k++) {
+				qreal real = (*ca)[i][j][k].real();
+				if (real > max) {
+					max = real;
+				} else if (real < min) {
+					min = real;
+				}
+			}
+		}
+
+		for (unsigned int j = 0; j < ca->shape()[1]; j++) {
+			for (unsigned int k = 0; k < ca->shape()[2]; k++) {
+				qreal p = ((*ca)[i][j][k].real() - min) / (max - min) * 255.0;
+				{
+					QVector3D oldPixel = cp.pixel(k, j, result);
+					QVector3D newPixel;
+					switch (i) {
+						case 0:
+							newPixel.setX(p);
+							break;
+						case 1:
+							newPixel.setY(p);
+							break;
+						case 2:
+							newPixel.setZ(p);
+							break;
+						default:
+							break;
+					}
+					cp.setPixel(k, j, result, cp.merge(oldPixel, newPixel));
+				}
+			}
+		}
+	}
+	PhotoWindow *pw = new PhotoWindow(result, windowTitle() + ", IFFT", qobject_cast<QWidget*>(parent()->parent()));
+	pw->show();
 }
--- transformwindow.h
+++ transformwindow.h
@@ -2,6 +2,7 @@
 #define TRANSFORMWINDOW_H
 
 #include "displaywindow.h"
+#include "complexarray.h"
 
 #include <QHash>
 #include <QUuid>
@@ -17,7 +18,7 @@ class TransformWindow : public DisplayWindow
 	Q_OBJECT
 
 public:
-	explicit TransformWindow(QImage magnitude, QImage phase, QString title, QWidget *parent = 0);
+	explicit TransformWindow(QImage magnitude, QImage phase, ComplexArray *ca, QString title, QWidget *parent = 0);
 	~TransformWindow();
 
 private:
@@ -27,11 +28,13 @@ private:
 	Ui::TransformWindow *ui;
 	QImage mMagnitudeImage;
 	QImage mPhaseImage;
+	ComplexArray *mCA;
 	QHash<QUuid, FilterInterface *> mFiltersHash;
 	QMenu *mFiltersMenu;
 
 private slots:
 	void applyFilter(QAction *action);
+	void invert();
 };
 
 #endif // TRANSFORMWINDOW_H
